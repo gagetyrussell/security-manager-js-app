ff53d6cd3d3d6467f6de928d438dace3311df172
gagetyrussell <gagetyrussell@gmail.com>
2020-01-03 19:26:14 -0600

    changed config to match rest of security app tools

--- src/config.js
+++ src/config.js
@@ -1,15 +1,11 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
-  MGR_API_URL: "https://ykqnyciief.execute-api.us-east-1.amazonaws.com/dev",
+  MGR_API_URL: "https://7teqpb3w39.execute-api.us-east-1.amazonaws.com/dev",
   LOCAL_MGR_API_URL: "http://localhost:5000/",
-  apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://qcp3xrmpmj.execute-api.us-east-1.amazonaws.com/dev/"
-  },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_AhleHc0IP",
-    APP_CLIENT_ID: "7snnj35ibuk6nhd44kgi377m63",
-    IDENTITY_POOL_ID: "us-east-1:2d5d7933-94f5-4ae9-bee5-a46ac7c65352"
+    USER_POOL_ID: "us-east-1_ci3WukOFZ",
+    APP_CLIENT_ID: "22r90uso7c622aqg6eiuvlq67p",
+    IDENTITY_POOL_ID: "us-east-1:0f891aca-48ca-42f4-ae51-92a8757a6a6f"
   }
 };
